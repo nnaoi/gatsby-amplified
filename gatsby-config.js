a60668c09dd115c82441a88c5a68d7773af4b8c4
nnaoi <naoinamiki1217@gmail.com>
2021-03-06 15:01:41 +0900

    netlify functionsしかデプロイする設定になっていたため修正。

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+const { createProxyMiddleware } = require("http-proxy-middleware") //v1.x.x
+// Use implicit require for v0.x.x of 'http-proxy-middleware'
+// const proxy = require('http-proxy-middleware')
+// be sure to replace 'createProxyMiddleware' with 'proxy' where applicable
 
 module.exports = {
   siteMetadata: {
@@ -102,4 +106,17 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
+  // for avoiding CORS while developing Netlify Functions locally
+  // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
+  developMiddleware: app => {
+    app.use(
+      "/.netlify/functions/",
+      createProxyMiddleware({
+        target: "http://localhost:9000",
+        pathRewrite: {
+          "/.netlify/functions/": "",
+        },
+      })
+    )
+  },
 }
